2c9579c81aef882241cbe3a562673076297640b5
Jukka Arola <jukka.arola@gmail.com>
2015-10-29 10:21:53 +0200

    transformations
    Added transformation files and targets-nuget

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ClassLibrary1
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Lol()
+        {
+            string expected = "value";
+
+            var actual = ConfigurationManager.AppSettings["SomeValue"];
+            
+            Assert.AreEqual(expected, actual, "should be lol was " + actual);
+        }
+    }
+}
